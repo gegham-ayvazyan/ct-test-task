a697ff1923aff38c505396f486a81ac12a279dee
Gegham Ayvazyan <gegham.aivazyan@gmail.com>
2018-06-29 22:21:35 +0400

    Added routes and controller CRUD for products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $models = Product::get();
+        return view('product.index', compact('models'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $model = new Product();
+        return view('product.create', compact('model'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:150',
+            'quantity' => 'integer',
+            'price' => 'float',
+        ]);
+        $model = new Product();
+        $model->fill($request->only(['name', 'quantity', 'price']));
+        $model->save();
+
+        return redirect()->route('product.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $model = Product::findOrFail($id);
+
+        return view('product.show', compact('model'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $model = Product::findOrFail($id);
+
+        return view('product.edit', compact('model'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:150',
+            'quantity' => 'integer',
+            'price' => 'float',
+        ]);
+        $model = Product::findOrFail($id);
+        $model->fill($request->only(['name', 'quantity', 'price']));
+        $model->save();
+
+        return redirect()->route('product.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $model = Product::findOrFail($id);
+        $model->delete();
+
+        return redirect()->route('product.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'product'], function() {
+    Route::get('/', 'ProductController@index')->name('product.index');
+    Route::get('create', 'ProductController@create')->name('product.create');
+    Route::post('/', 'ProductController@store')->name('product.store');
+    Route::get('edit/{id}', 'ProductController@edit')->name('product.edit');
+    Route::put('{id}', 'ProductController@update')->name('product.update');
+    Route::get('{id}', 'ProductController@show')->name('product.show');
+    Route::delete('{id}', 'ProductController@destroy')->name('product.destroy');
+});
